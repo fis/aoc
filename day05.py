1f201b50df3bb632218d438eb0904feabc45ad03
Heikki Kallasjoki <fis@zem.fi>
2019-12-06 01:15:23 +0000

    Clean up the intcode interpreter a bunch.

--- day05.py
+++ day05.py
@@ -3,7 +3,12 @@
 import intcode
 import sys
 
+prog = intcode.load('day05-input.txt' if len(sys.argv) < 2 else sys.argv[1])
+
 # part 1
 
-prog = intcode.load('day05-input.txt' if len(sys.argv) < 2 else sys.argv[1])
-intcode.run(prog)
+intcode.run(prog, stdin=[1])
+
+# part 2
+
+intcode.run(prog, stdin=[5])
--- intcode.py
+++ intcode.py
@@ -1,56 +1,15 @@
 import collections
 import enum
 
-Opcode = collections.namedtuple('Opcode', 'name nargs action')
+def load(path):
+    with open(path) as f:
+        return [int(i) for i in f.readline().split(',')]
 
-opcodes = {
-    1: Opcode(
-        name='add',
-        nargs=3,
-        action=lambda p, a: write(p, a[2], read(p, a[0]) + read(p, a[1])),
-    ),
-    2: Opcode(
-        name='mul',
-        nargs=3,
-        action=lambda p, a: write(p, a[2], read(p, a[0]) * read(p, a[1])),
-    ),
-    3: Opcode(
-        name='in',
-        nargs=1,
-        action=lambda p, a: write(p, a[0], prompt()),
-    ),
-    4: Opcode(
-        name='out',
-        nargs=1,
-        action=lambda p, a: output(read(p, a[0])),
-    ),
-    # TODO better
-    5: Opcode(
-        name='jnz',
-        nargs=2,
-        action='jnz',
-    ),
-    6: Opcode(
-        name='jz',
-        nargs=2,
-        action='jz',
-    ),
-    7: Opcode(
-        name='setlt',
-        nargs=3,
-        action=lambda p, a: write(p, a[2], int(read(p, a[0]) < read(p, a[1]))),
-    ),
-    8: Opcode(
-        name='seteq',
-        nargs=3,
-        action=lambda p, a: write(p, a[2], int(read(p, a[0]) == read(p, a[1]))),
-    ),
-    99: Opcode(
-        name='halt',
-        nargs=0,
-        action=None,
-    ),
-}
+def run(prog, stdin=None, trace=False):
+    vm = VM(prog)
+    vm.run(stdin=stdin, trace=trace)
+
+Opcode = collections.namedtuple('Opcode', 'name nargs action jump', defaults=(0, None, False))
 
 Arg = collections.namedtuple('Arg', 'mode value')
 
@@ -58,59 +17,119 @@ class ArgMode(enum.Enum):
     INDIRECT = 0
     IMMEDIATE = 1
 
-def load(path):
-    with open(path) as f:
-        return [int(i) for i in f.readline().split(',')]
+    @classmethod
+    def prefix(cls, mode):
+        return {
+            ArgMode.INDIRECT: '@',
+            ArgMode.IMMEDIATE: '',
+        }[mode]
 
-def run(prog):
-    ip = 0
-    while True:
-        opcode, args = decode(prog, ip)
-        #print('{}: {}'.format(ip, encode(opcode, args)))
-        if opcode.action is None:
-            return
-        # TODO better
-        if opcode.action == 'jnz':
-            if read(prog, args[0]) != 0: ip = read(prog, args[1])
-            else: ip += 3
-        elif opcode.action == 'jz':
-            if read(prog, args[0]) == 0: ip = read(prog, args[1])
-            else: ip += 3
+class VM:
+    def __init__(self, prog):
+        self._ip = 0
+        self._prog = list(prog)
+        self._stdin = None
+        pass
+
+    def run(self, ip=0, stdin=None, trace=False):
+        self._ip = ip
+        self._stdin = stdin
+        while True:
+            opcode, args = self._fetch(self._ip)
+            if trace: self._trace(opcode, args)
+            if opcode.action is None:
+                return
+            opcode.action(self, *args)
+            if not opcode.jump:
+                self._ip += 1 + opcode.nargs
+
+    def _trace(self, opcode, args):
+        out = '{:-4d}: {}'.format(self._ip, opcode.name)
+        for arg in args:
+            out += ' {}{}'.format(ArgMode.prefix(arg.mode), arg.value)
+        print(out)
+
+    def _fetch(self, ip):
+        if ip < 0 or ip >= len(self._prog):
+            raise IndexError('fetch ip {} beyond [0, {})'.format(ip, len(self._prog)))
+        op = self._prog[ip]
+        opcode = VM._opcodes.get(op % 100, None)
+        if opcode is None:
+            raise IndexError('invalid opcode: {} ({})'.format(op % 100, op))
+        args, argpos = [], 100
+        for i in range(opcode.nargs):
+            if ip + 1 + i >= len(self._prog):
+                raise IndexError('fetch arg {} beyond [0, {})'.format(ip + 1 + i, len(self._prog)))
+            args.append(Arg(
+                mode=ArgMode(op // argpos % 10),
+                value=self._prog[ip + 1 + i],
+            ))
+            argpos *= 10
+        return opcode, args
+
+    def _read(self, arg):
+        if arg.mode == ArgMode.INDIRECT:
+            return self._prog[arg.value]
+        elif arg.mode == ArgMode.IMMEDIATE:
+            return arg.value
         else:
-            opcode.action(prog, args)
-            ip += 1 + opcode.nargs
+            raise RuntimeError('invalid read arg: ' + repr(arg))
 
-def decode(prog, ip):
-    op = prog[ip]
-    opcode = opcodes[op % 100]
-    args, argpos = [], 100
-    for i in range(opcode.nargs):
-        args.append(Arg(
-            mode=ArgMode(op // argpos % 10),
-            value=prog[ip+1+i],
-        ))
-        argpos *= 10
-    return opcode, args
+    def _write(self, arg, n):
+        if arg.mode == ArgMode.INDIRECT:
+            self._prog[arg.value] = n
+        else:
+            raise RuntimeError('invalid write arg: ' + repr(arg))
 
-def encode(opcode, args):
-    out = opcode.name
-    for arg in args:
-        out += ' '
-        out += ' {}{}'.format('@' if arg.mode == ArgMode.INDIRECT else '', arg.value)
-    return out
+    def _input(self):
+        if self._stdin is None:
+            return int(input('? '))
+        elif self._stdin:
+            n = self._stdin[0]
+            self._stdin = self._stdin[1:]
+            print('? -> {}'.format(n))
+            return n
+        else:
+            raise RuntimeError('read past provided input')
 
-def read(prog, arg):
-    if arg.mode == ArgMode.INDIRECT:
-        return prog[arg.value]
-    elif arg.mode == ArgMode.IMMEDIATE:
-        return arg.value
+    def _op_add(self, a1, a2, dst):
+        self._write(dst, self._read(a1) + self._read(a2))
 
-def write(prog, arg, n):
-    if arg.mode == ArgMode.INDIRECT:
-        prog[arg.value] = n
+    def _op_mul(self, a1, a2, dst):
+        self._write(dst, self._read(a1) * self._read(a2))
 
-def prompt():
-    return int(input('? '))
+    def _op_in(self, dst):
+        self._write(dst, self._input())
 
-def output(n):
-    print(n)
+    def _op_out(self, a):
+        print(self._read(a))
+
+    def _op_jnz(self, a, tgt):
+        if self._read(a) != 0:
+            self._ip = self._read(tgt)
+        else:
+            self._ip += 3
+
+    def _op_jz(self, a, tgt):
+        if self._read(a) == 0:
+            self._ip = self._read(tgt)
+        else:
+            self._ip += 3
+
+    def _op_setlt(self, a1, a2, dst):
+        self._write(dst, int(self._read(a1) < self._read(a2)))
+
+    def _op_seteq(self, a1, a2, dst):
+        self._write(dst, int(self._read(a1) == self._read(a2)))
+
+    _opcodes = {
+        1:  Opcode(name='add',   nargs=3, action=_op_add),
+        2:  Opcode(name='mul',   nargs=3, action=_op_mul),
+        3:  Opcode(name='in',    nargs=1, action=_op_in),
+        4:  Opcode(name='out',   nargs=1, action=_op_out),
+        5:  Opcode(name='jnz',   nargs=2, action=_op_jnz, jump=True),
+        6:  Opcode(name='jz',    nargs=2, action=_op_jz, jump=True),
+        7:  Opcode(name='setlt', nargs=3, action=_op_setlt),
+        8:  Opcode(name='seteq', nargs=3, action=_op_seteq),
+        99: Opcode(name='halt'),
+    }
