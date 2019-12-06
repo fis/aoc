8d55c148b7c7655d2d8b1fcffb2a690078f9f050
Heikki Kallasjoki <fis@zem.fi>
2019-12-06 01:15:43 +0000

    Test harness improvements.

--- test.py
+++ test.py
@@ -2,24 +2,32 @@
 
 import os.path
 import subprocess
+import sys
 
-day = 1
-while True:
-    cmd = './day{:02}.py'.format(day)
-    if not os.path.exists(cmd):
-        break
+if len(sys.argv) > 1:
+    tests = sys.argv[1:]
+else:
+    tests = []
+    for day in range(1, 26):
+        cmd = './day{:02}.py'.format(day)
+        if not os.path.exists(cmd):
+            break
+        tests.append(cmd)
 
-    with open('day{:02}-output.txt'.format(day)) as f:
+for cmd in tests:
+    cmd_base, ext = os.path.splitext(cmd)
+    want_file = cmd_base + '-output.txt'
+    if not os.path.exists(want_file):
+        raise RuntimeError('no known good output: ' + want_file)
+    with open(want_file) as f:
         want = f.read()
 
     got = subprocess.run([cmd], check=True, capture_output=True, encoding='utf-8').stdout
 
     if got == want:
-        print('day{} ... pass'.format(day))
+        print('{} ... pass'.format(cmd))
     else:
-        print('day{} ... FAIL'.format(day))
+        print('{} ... FAIL'.format(cmd))
         print('got:\n---\n{}\n---'.format(got))
         print('want:\n---\n{}\n---'.format(want))
         break
-
-    day += 1
