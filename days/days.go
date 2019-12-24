5dca0a1937365c18462dc0cf0c8b05b24c620ddd
Heikki Kallasjoki <fis@zem.fi>
2019-12-24 01:54:46 +0000

    Solve day 3.

--- days/days.go
+++ days/days.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/fis/aoc2019-go/day01"
 	"github.com/fis/aoc2019-go/day02"
+	"github.com/fis/aoc2019-go/day03"
 	"github.com/fis/aoc2019-go/day04"
 	"github.com/fis/aoc2019-go/day05"
 	"github.com/fis/aoc2019-go/day11"
@@ -16,6 +17,7 @@ import (
 var solvers = map[int]func(string) ([]string, error){
 	1:  day01.Solve,
 	2:  day02.Solve,
+	3:  day03.Solve,
 	4:  day04.Solve,
 	5:  day05.Solve,
 	11: day11.Solve,
