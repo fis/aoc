3e4cdcfd7d667d7f32ca575990be74a46f290df4
Heikki Kallasjoki <fis@zem.fi>
2023-12-02 16:41:30 +0000

    Add Z80 solution for 2023 day 2.

--- z80/2023/generate.go
+++ z80/2023/generate.go
@@ -4,3 +4,5 @@ package y2023
 //go:generate -command asm z80asm -I ../lib
 //go:generate asm -o day01-1.bin day01-1.z80
 //go:generate asm -o day01-2.bin day01-2.z80
+//go:generate asm -o day02-1.bin day02-1.z80
+//go:generate asm -o day02-2.bin day02-2.z80
--- z80/cmd/z80run/main.go
+++ z80/cmd/z80run/main.go
@@ -10,13 +10,19 @@ import (
 	"github.com/fis/aoc/z80/z80ex"
 )
 
-const usage = `usage: z80run prog.bin
+var (
+	bound = flag.Int64("bound", -1, "only emulate up to (approximately) N t-states")
+)
+
+const usage = `usage: z80run [flags] prog.bin
+Optional flags:
 `
 
 func main() {
 	flag.Parse()
 	if flag.NArg() != 1 {
-		fmt.Print(usage)
+		fmt.Fprint(os.Stderr, usage)
+		flag.PrintDefaults()
 		os.Exit(1)
 	}
 	prog := flag.Arg(0)
@@ -34,8 +40,14 @@ func main() {
 	bufIn, bufOut := bufio.NewReader(os.Stdin), bufio.NewWriter(os.Stdout)
 	cpu := z80ex.NewCPU()
 	cpu.WriteMem(bin, 0)
-	steps := cpu.Run(bufIn, bufOut)
-	bufOut.Flush()
 
+	var steps uint64
+	if *bound <= 0 {
+		steps = cpu.Run(bufIn, bufOut)
+	} else {
+		steps, _ = cpu.RunBounded(bufIn, bufOut, uint64(*bound))
+	}
+
+	bufOut.Flush()
 	fmt.Printf("# T = %d\n", steps)
 }
